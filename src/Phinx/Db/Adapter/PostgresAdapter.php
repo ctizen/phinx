18ea192cfe3fe35c549a2adfd0ce76f3d2576051
Oleg Klimenko <o.klimenko@2gis.ru>
2017-05-14 23:07:18 +0300

    Fix quoting again

--- src/Phinx/Db/Adapter/PostgresAdapter.php
+++ src/Phinx/Db/Adapter/PostgresAdapter.php
@@ -994,8 +994,8 @@ class PostgresAdapter extends PdoAdapter implements AdapterInterface
 
         return sprintf(
             'COMMENT ON COLUMN %s.%s IS %s;',
-            $this->quoteTableName($tableName),
-            $column->getName(),
+            $this->quoteSchemaName($tableName),
+            $this->quoteColumnName($column->getName()),
             $comment
         );
     }
